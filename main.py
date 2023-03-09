06f475154a404bd1dea39a2ac561f9fbafe2eae0
prestante <prestante@yandex.ru>
2023-03-09 17:53:09 +0100

    init commit into new repo created(??) from pycharm

---
+++ main.py
@@ -0,0 +1,35 @@
+from rich import print
+from rich.console import Console
+from rich import inspect
+from rich.table import Table
+
+print("[bold magenta]Hello[/bold magenta] [i yellow]World![/i yellow]")
+
+console = Console()
+console.print("Hello", "World!", ":smiley:", style="bold red")
+
+my_list = ["foo", "bar"]
+inspect(my_list, methods=False)  # inspection of a class given
+
+table = Table(show_header=True, header_style="bold magenta")
+table.add_column("Date", style="dim", width=12)
+table.add_column("Title")
+table.add_column("Production Budget", justify="right")
+table.add_column("Box Office", justify="right")
+table.add_row(
+    "Dec 20, 2019", "Star Wars: The Rise of Skywalker", "$275,000,000", "$375,126,118"
+)
+table.add_row(
+    "May 25, 2018",
+    "[red]Solo[/red]: A Star Wars Story",
+    "$275,000,000",
+    "$393,151,347",
+)
+table.add_row(
+    "Dec 15, 2017",
+    "Star Wars Ep. VIII: The Last Jedi",
+    "$262,000,000",
+    "[bold]$1,332,539,889[/bold]",
+)
+
+console.print(table)
